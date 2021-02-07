591dbf9c6fe403fb43ba850ff56e07a3dbc4c6df
Byeonggyu <byeonggyu303@gmail.com>
2021-02-08 01:27:03 +0900

    init

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 1. 로딩 성능 최적화
 2. 렌더링 최적화
+3. 이미지 프리로드
+4. 컴포넌트 프리로드
+5. 컴포넌트 레이지 로딩
 
 ## webpack-bundle-analyzer
 
@@ -32,7 +35,83 @@ a_kw=imgix&hsa_cam=9210499657&gclid=Cj0KCQiAvP6ABhCjARIsAH37rbTScnL4y1YSBZ2aBPIC
 
 ### 2. Deflate
 
+## 렌더링 최적화
+
+- 프레임 드랍에 의한 쟁크 현상을 피해야한다.
+- 리플로우 리페인트를 최소화한다.
+- 리플로우 리페인트가 생기는 css 속성 변화를 최소화한다.
+- critical rendering path, Pixel Pipline
+
+### 랜더링 과정
+
+layout 과정과, paint과정을 최소화해야한다.
+
+1. dom + cssom
+2. render tree
+3. layout
+4. paint
+5. composite
+
+width, heigt는 전 과정을 밟는다.
+color는 layout과정이 생략된다.
+
+### reflow와 repaint를 모두 피하는 방법
+
+gpu가 관여하는 속성( trasform, opacity)을 이용한다.  
+layout과 paint가 생략된다.(reflow, repaint)
+
+### Reflow발생
+
+위치와 크기가 변경된다.
+
+- position
+- width
+- height
+- left
+- bottom
+- margin
+- padding
+- border
+- border-width
+- diplay
+- float
+- font-family
+- font-size
+- font-weight
+- line-height
+- overflow
+- text-align
+- vertical-align
+
+### repaint 발생
+
+위치는 바꾸지 않고 크기만 바꾼다.
+
+- background-color
+- background-image
+- background-position
+- background-repeat
+- background-size
+- border
+- box-shadow
+- color
+- line-style
+- online
+- text-decoration
+
+### 둘 다 하지 생략
+
+#### transform
+
+- scale
+- scaleX
+- translate
+- rotate
+
+#### opacity
+
 ## ref
 
 - [repository](https://github.com/performance-lecture/lecture-1)
 - [imgix](https://www.imgix.com/?utm_term=imgix&utm_campaign=adwords-branded&utm_source=adwords&utm_medium=ppc&hsa_tgt=kwd-347244981599&hsa_grp=98890898611&hsa_src=g&hsa_net=adwords&hsa_mt=p&hsa_ver=3&hsa_ad=456649958299&hsa_acc=8534109361&hs
+- [transform](https://developer.mozilla.org/ko/docs/Web/CSS/transform)
---
+++ lecture-2/src/App.js
@@ -0,0 +1,73 @@
+import React, { useState, Suspense, lazy, useEffect } from "react";
+import styled from "styled-components";
+import Header from "./components/Header";
+import InfoTable from "./components/InfoTable";
+import SurveyChart from "./components/SurveyChart";
+import Footer from "./components/Footer";
+
+/**
+ * @description factory pattern
+ * @param {*} importFunction
+ */
+function lazyWidthPreload(importFunction) {
+  const Component = lazy(importFunction);
+  Component.preload = importFunction;
+
+  return Component;
+}
+
+function imagePreload(src) {
+  const img = new Image();
+  img.src = src;
+}
+
+const LazyImageModal = lazyWidthPreload(() =>
+  import("./components/ImageModal")
+);
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const hanldeClick = () => {
+    setShowModal(true);
+  };
+
+  useEffect(() => {
+    // prelaod
+    LazyImageModal.preload();
+
+    imagePreload("");
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+      <InfoTable />
+      <ButtonModal onClick={hanldeClick}>올림픽 사진 보기</ButtonModal>
+      <SurveyChart />
+      <Footer />
+      <Suspense fallback={() => null}>
+        {showModal ? (
+          <LazyImageModal
+            closeModal={() => {
+              setShowModal(false);
+            }}
+          />
+        ) : null}
+      </Suspense>
+    </div>
+  );
+}
+
+const ButtonModal = styled.button`
+  border-radius: 30px;
+  border: 1px solid #999;
+  padding: 12px 30px;
+  background: none;
+  font-size: 1.1em;
+  color: #555;
+  outline: none;
+  cursor: pointer;
+`;
+
+export default App;
